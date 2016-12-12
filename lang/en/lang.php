2a4338067b54a49c740f878e82425f5e2ba4efd4
uitlaber <uitlaber@gmail.com>
2016-12-13 00:07:25 +0600

    fixed lang

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -2,7 +2,7 @@
 
 return [
 	'plugin' => [
-		'name' => 'Toaster notify',
-		'description' => 'Simple toaster notification plugin'
+		'name' => 'Toastr notify',
+		'description' => 'Simple toastr notification plugin'
 	]	
 ];
